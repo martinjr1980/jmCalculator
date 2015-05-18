1f622cb6697e1f44de19ca554e82ad36cad1c305
martinjr1980 <martinjr1980@gmail.com>
2015-05-18 15:33:40 -0700

    Works for all operators except when combining subtract with multply, divide or modulus

--- index.php
+++ index.php
@@ -1,59 +1,71 @@
 <?php
 
-class Calculator {
-	public function evaluate($string) {
+class Calculator
+{
+	public function evaluate($string)
+	{
 		$pattern = "/([-+*%\/])/";
 		$array = preg_split($pattern, $string, -1, PREG_SPLIT_DELIM_CAPTURE);
 		$len = count($array);
 		$total = 0;
-		for ($i = 0; $i < $len; $i++) {
-			if ($array[$i] === '+') {
-				$a = (int) $array[$i-1];
-				$b = (int) $array[$i+1];
-				array_shift($array);
-				array_shift($array);
-				$total = $this->add($a, $b);
-				$array[0] = $total;
-				$i = 0;
-				$len -= 2;
-			} elseif ($array[$i] === '-') {
-				$a = (int) $array[$i-1];
-				$b = (int) $array[$i+1];
-				array_shift($array);
-				array_shift($array);
-				$total = $this->subtract($a, $b);
-				$array[0] = $total;
-				$i = 0;
-				$len -= 2;
+		var_dump($array);
+		for ($i = 0; $i < $len; $i++)
+		{
+			if ($array[$i] === '*' || $array[$i] === '/' || $array[$i] === '%')
+			{
+				$a = floatval($array[$i-1]);
+				$b = floatval($array[$i+1]);
+				$total = $this->operators($array[$i], $a, $b);
+				$array[$i-1] = 0;
+				$array[$i] = '+';
+				$array[$i+1] = $total;
+				var_dump($array);
+			}
+		}
+
+		for ($j = 0; $j < $len; $j++)
+		{
+			if ($array[$j] === '+' || $array[$j] === '-')
+			{
+				$a = floatval($array[$j-1]);
+				$b = floatval($array[$j+1]);
+				$total = $this->operators($array[$j], $a, $b);
+				$array[$j-1] = 0;
+				$array[$j] = '+';
+				$array[$j+1] = $total;
+				var_dump($array);
 			}
 		}
 		echo $total;
-	}
+	}	
 
-	function add($a, $b) {
-		return $a + $b;
-	}
-
-	function subtract($a, $b) {
-		return $a - $b;
-	}
-
-	function multiply($a, $b) {
-		return $a * $b;
-	}
-
-	function divide($a, $b) {
-		return $a / $b;
-	}
-
-	function modulus($a, $b) {
-		return $a % $b;
+	function operators($string, $a, $b)
+	{
+		if ($string === '+')
+		{
+			return $a + $b;
+		}
+		elseif ($string === '-')
+		{
+			return $a - $b;
+		}
+		elseif ($string === '*')
+		{
+			return $a * $b;
+		}
+		elseif ($string === '/')
+		{
+			return $a / $b;
+		}
+		elseif ($string === '%')
+		{
+			return $a % $b;
+		}
 	}
 }
 
 $calculator = new Calculator();
-
-$calculator->evaluate('3 + 5 + 1 + 9 - 18 + 4');
-
-
+$calculator->evaluate('1+2*3-4%5+6/7-8*9%10');
+// $eval = 1+2*3-4%5+6/7-8*9%10;
+// echo $eval;
 ?>
