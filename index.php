071aa5aba51617a677fe992065ac57548f906fad
martinjr1980 <martinjr1980@gmail.com>
2015-05-18 13:18:01 -0700

    First iteration of combo of add and subtract operations

--- index.php
+++ index.php
@@ -1,6 +1,35 @@
 <?php
 
 class Calculator {
+	public function evaluate($string) {
+		$pattern = "/([-+*%\/])/";
+		$array = preg_split($pattern, $string, -1, PREG_SPLIT_DELIM_CAPTURE);
+		$len = count($array);
+		$total = 0;
+		for ($i = 0; $i < $len; $i++) {
+			if ($array[$i] === '+') {
+				$a = (int) $array[$i-1];
+				$b = (int) $array[$i+1];
+				array_shift($array);
+				array_shift($array);
+				$total = $this->add($a, $b);
+				$array[0] = $total;
+				$i = 0;
+				$len -= 2;
+			} elseif ($array[$i] === '-') {
+				$a = (int) $array[$i-1];
+				$b = (int) $array[$i+1];
+				array_shift($array);
+				array_shift($array);
+				$total = $this->subtract($a, $b);
+				$array[0] = $total;
+				$i = 0;
+				$len -= 2;
+			}
+		}
+		echo $total;
+	}
+
 	function add($a, $b) {
 		return $a + $b;
 	}
@@ -23,7 +52,8 @@ class Calculator {
 }
 
 $calculator = new Calculator();
-echo $calculator->modulus(1, 2);
+
+$calculator->evaluate('3 + 5 + 1 + 9 - 18 + 4');
 
 
 ?>
